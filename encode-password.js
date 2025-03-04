63772a907fb2c10afecf58c2ed49ee060b89467f
joel <joel.melo171@gmail.com>
2025-03-03 22:50:21 -0300

    novo projeto

--- encode-password.js
+++
@@ -1,6 +0,0 @@
-// Script para codificar caracteres especiais na senha do MongoDB
-const password = "joe12823134719"; // Coloque sua senha real aqui
-const encodedPassword = encodeURIComponent(password);
-console.log("Senha original:", password);
-console.log("Senha codificada para URL:", encodedPassword);
-console.log("Use a senha codificada no arquivo .env");
